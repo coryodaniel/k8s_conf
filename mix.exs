394acee1d17478c4e167c8d1551e702f8004bf55
Cory O'Daniel <cory@coryodaniel.com>
2018-12-19 13:07:52 -0800

    adding coveralls/travis task

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule K8sConf.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.travis": :test
       ],
       docs: [
         extras: ["README.md"],
