edd1d3af2bc7383e855a38987f22e87e2393acd9
Cory O'Daniel <cory@coryodaniel.com>
2018-12-19 13:02:16 -0800

    removing 1.6 from travis because ex_doc

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule K8sConf.MixProject do
       app: :k8s_conf,
       description: "k8s_conf parses Kubernetes config files and generates HTTP headers and options for authenticating to the Kubernetes API.",
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
